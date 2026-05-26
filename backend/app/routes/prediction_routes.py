d412d8647194310f6a8f1c659080f3a7e50b7e16
atikafarah <atikafarah1317@gmail.com>
2026-05-26 10:44:21 +0700

    tambah endpoint validation

--- backend/app/routes/prediction_routes.py
+++ backend/app/routes/prediction_routes.py
@@ -48,9 +48,8 @@ def get_predictions():
         cur.close()
         conn.close()
 
-
-@router.get("/latest")
-def get_latest_predictions():
+@router.get("/validation")
+def get_prediction_validation(weight: float):
 
     conn = connect_db()
     cur = conn.cursor()
@@ -58,18 +57,28 @@ def get_latest_predictions():
     try:
 
         cur.execute("""
-            SELECT
-                id,
-                prediction_date,
-                target_date,
-                weight,
-                predicted_price,
-                predicted_delta,
-                model_version
-            FROM predictions
-            ORDER BY created_at DESC
-            LIMIT 10
-        """)
+            SELECT DISTINCT ON (p.target_date)
+
+                p.target_date,
+                g.sell_price AS actual_price,
+                p.predicted_price,
+                p.prediction_date
+
+            FROM predictions p
+
+            JOIN gold_prices g
+                ON DATE(g.updated_at) = p.target_date
+                AND g.weight = p.weight
+
+            WHERE p.weight = %s
+            AND p.prediction_date < p.target_date
+
+            ORDER BY
+                p.target_date,
+                p.prediction_date DESC
+        """, (
+            weight,
+        ))
 
         rows = cur.fetchall()
 
@@ -78,13 +87,19 @@ def get_latest_predictions():
         for row in rows:
 
             data.append({
-                "id": row[0],
-                "prediction_date": row[1],
-                "target_date": row[2],
-                "weight": row[3],
-                "predicted_price": row[4],
-                "predicted_delta": row[5],
-                "model_version": row[6]
+
+                "tanggal":
+                    row[0].strftime("%d/%m"),
+
+                "aktual":
+                    float(row[1]),
+
+                "prediksi":
+                    float(row[2]),
+
+                "prediction_date":
+                    row[3].strftime("%Y-%m-%d")
+
             })
 
         return data
@@ -92,4 +107,4 @@ def get_latest_predictions():
     finally:
 
         cur.close()
-        conn.close()
+        conn.close() 
